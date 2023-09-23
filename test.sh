b145d8aeb96497fdbf215887be848607eee271ef
AnubhabDebnathAgnikul <135800433+AnubhabDebnathAgnikul@users.noreply.github.com>
2023-09-23 12:54:05 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ result=$(./main)
 # Check the exit status of the C program
 if [ $? -eq 1 ]; then
     echo "System Validation Success"
-    ./health_check
+    #./health_check  
 else
     echo "Failure, abort!"
 fi
