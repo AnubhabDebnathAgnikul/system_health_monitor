2a8fbcb36ecaca62f3a2a5a773a51769512505f4
Sai Thilak <saithilak@agnikul.in>
2023-12-14 01:14:51 +0530

    Tested the test.sh with rpi.

--- test.sh
+++ test.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-designation="FC"
+designation="EC"
 
 if [ "$designation" = "FC" ];
 then
-    path='/root/fc_test/'
+    path='/tmp/fc_test/'
 fi
 
 if [ "$designation" = "EC" ]; 
 then
-    path='/root/ec_test/'
+    path='/tmp/ec_test/'
 fi
 
-result=$(./health_check)
-
-# Check the exit status of the C program
-# if [ $? -eq 1 ]; then
-#     echo "System Validation Success"
-#     #./health_check  
-# else
-#     echo "Failure, abort!"
-#     exit 1
-# fi
-
-# # Start PTP.
-# if [ "$designation" = "FC" ]; then
-#     ptpd2 -i switch -s -U -S $path/files/ptp_log_stream 1>/dev/null 2>/dev/null
-# fi
-
-# if [ "$designation" = "EC" ]; then
-#     ptpd2 -i switch -s -U -S $path/files/ec/ptp_log_stream 1>/dev/null 2>/dev/null
-# fi
-
-# # Run the application test and capture its output
-# result=$(./app_test /root/fc_test/)
+# logger -t "ST" "Starting System Test...."
+# result=$(/root/system_test)
+# logger -t "ST" "Starting System Test Complete"
 
 # # Check the exit status of the C program
 # if [ $? -eq 1 ]; then
-#     echo "Application Validation Success"
+#     logger -t "ST" "System Validation Success"
 #     #./health_check  
 # else
-#     echo "Failure, abort!"
-#     exit 
+#     logger -t "ST" "Failure, abort!"
+#     exit 1
 # fi
+
+# Start PTP.
+if [ "$designation" = "FC" ]; then
+    ptpd2 -i switch -s -U -S $path/files/ptp_log_stream 1>/dev/null 2>/dev/null
+fi
+
+if [ "$designation" = "EC" ]; then
+    ptpd2 -i eth0 -s -U -S $path/files/ec/ptp_log_stream 1>/dev/null 2>/dev/null
+fi
+
+logger -t "ST" "Starting Application Test...."
+# Run the application test and capture its output
+result=$(./app_test $path $designation )
+logger -t "ST" "Starting Application Complete"
+
+# Check the exit status of the C program
+if [ $? -eq 0 ]; then
+    logger -t "ST" "Application Validation Success"
+    #./health_check  
+else
+    logger -t "ST" "Failure, abort!"
+    exit 
+fi
